f0908f1e97b8cdb2e4af165a21551d7d7b8c2fe0
leolucky90 <leolucky90@gmail.com>
2021-10-24 19:33:45 +1100

    git rebase

---
+++ components/Item.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { View, Text, StyleSheet, Button } from 'react-native'
+export const Item = (props) => {
+  
+  return(
+    <View style={styles.container}>
+      <Text style={styles.text }>{props.text}</Text>
+      <Button title="delete" onPress={ () => props.delete( props.id ) } />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 10,
+    backgroundColor: 'lightgreen',
+    borderBottomColor: 'black',
+    borderBottomWidth: 1,
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  text: {
+    flex: 1,
+  },
+})
